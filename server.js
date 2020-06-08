ebb5e56b71ac375d24ff619dd90a4ffbd74ae523
Illich Rada <illich570@gmail.com>
2020-06-08 16:01:48 -0400

    [ADD] Connection to MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const Server = express();
 
 Server.use(express.static('public'));
 Server.use(cors());
+Server.use(bodyParser.json());
+Server.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, (err) => {
+  err
+    ? console.log("Error on connection")
+    : console.log("Connected Sucessfully");
+});
 
 Server.get('/',(req,res) =>{
   res.sendFile(__dirname + '/public/views/index.html');
